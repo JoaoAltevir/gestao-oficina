76c7c09f12506e0188b83e2771c85879d56c3eef
João Neto <j.altevirneto@gmail.com>
2024-10-11 10:15:08 -0300

    refact & feat: add on show for show all maintanences with a document not just id

--- src/controllers/maintenance-controller.js
+++ src/controllers/maintenance-controller.js
@@ -3,6 +3,12 @@ import Maintenance from "../models/maintenance-model.js";
 
 export const store = async (req,res) => {
     try {
+        req.body.totalCost = 0;
+        console.log(req.body.services.length)
+        for(let i = 0; i < req.body.services.length;i++){
+            console.log("entrei")
+            req.body.totalCost += req.body.services[i].price
+        };
         await Maintenance.create(req.body);
         res.json();
     } catch (error) {
@@ -21,7 +27,7 @@ export const index = async (req, res) => {
 
 export const show = async (req,res) => {
     try {
-        const content = await Maintenance.findById(req.params.id).exec();
+    const content = await Maintenance.findById(req.params.id).populate("vehicle", "workshop").exec();
         res.json(content); 
     } catch (error) {
         res.status(400).json(error);
--- src/controllers/vehicle-controller.js
+++ src/controllers/vehicle-controller.js
@@ -20,7 +20,7 @@ export const index = async (req, res) => {
 
 export const show = async (req,res) => {
     try {
-        const content = await Vehicle.findById(req.params.id).exec();
+        const content = await Vehicle.findById(req.params.id).populate("maintenances").exec();
         res.json(content); 
     } catch (error) {
         res.status(400).json(error);
